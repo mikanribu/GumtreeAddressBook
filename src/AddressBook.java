1cf8985d55128ae5732cb575ac83307abaa51ea5
Clementine Pages <clementine.pages@gmail.com>
2014-09-02 22:50:55 -0400

    Create List of Person

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class AddressBook {
 
+	protected List<Person> adBook = new ArrayList<Person>();
+	
+	
+	public static void main(String[] args) {
+		
+	}
 }
