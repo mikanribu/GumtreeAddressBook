ec3f757bcce7bf5f4803078db60645538aedf3c1
Clementine Pages <clementine.pages@gmail.com>
2014-09-02 22:53:55 -0400

    Create Person class, with attributes, Getters and Setters

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+
 import java.util.Date;
 
 
@@ -7,4 +8,49 @@ public class Person {
 	protected String lastName;
 	protected String gender;
 	protected Date date;
+	
+	public Person(String sName, String sGender, Date sDate)
+	{
+		String[] allName = sName.split(" ");
+		this.firstName = allName[0];
+		this.lastName = allName[1];
+		this.gender = sGender;
+		this.date = sDate;
+
+		
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
 }
